fc7ae7135fea15acd3df098ff1b13b213c58d8da
davidkatz <15Dkatz@shcp.edu>
2017-01-14 16:05:28 -0800

    update library

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-export function bake_cookie(name, value) {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.bake_cookie = bake_cookie;
+exports.read_cookie = read_cookie;
+exports.delete_cookie = delete_cookie;
+function bake_cookie(name, value) {
   var cookie = [name, '=', JSON.stringify(value), '; domain_.', window.location.host.toString(), '; path=/;'].join('');
   document.cookie = cookie;
 }
 
 // reads a cookie according to the given name
-export function read_cookie(name) {
+function read_cookie(name) {
   var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
   result = result != null ? JSON.parse(result[1]) : [];
   return result;
 }
 
-export function delete_cookie(name) {
+function delete_cookie(name) {
   document.cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain.', window.location.host.toString()].join('');
 }
